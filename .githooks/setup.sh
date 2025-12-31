3352fc449be9a89be6dadd3d2678e8cf7ffe905c
uselessgoddess <dodickgod@gmail.com>
2025-12-31 10:33:50 +0100

    remove githooks

--- .githooks/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Setup git hooks for local development
-# Run this once after cloning the repository
-
-git config core.hooksPath .githooks
-echo "Git hooks configured. Pre-commit hook will run fmt, clippy, and test."
